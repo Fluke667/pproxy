29d8d455ba0e9fbb809e276a950db8a27cf00804
Fluke667 <Fluke667@gmail.com>
2019-06-29 12:26:39 +0200

    Update pproxy.sh

--- pproxy.sh
+++ pproxy.sh
@@ -2,9 +2,7 @@
 
 pproxy -l socks4+socks5://:8090#$USER:$PASS &
 sleep 2 &
-exec pproxy -l http://:8080#$USER:$PASS
-#sleep 2 &
-#exec pproxy -l ss+ssr://:8070 -r ss+ssr://127.0.0.1:1080#$USER:$PASS
-#sleep 2 &
-#exec pproxy -l ss://:8060 -r ss://aes-256-gcm:yDRHHo1PjnolIVQHF3H4cpuL45udo7aHOco+Og==@127.0.0.1:1080 -vv
+exec pproxy -l http://:8080#$USER:$PASS &
+sleep 2 &
+exec pproxy -l ss://:8070 -r ss://aes-256-gcm!:yDRHHo1PjnolIVQHF3H4cpuL45udo7aHOco+Og==@127.0.0.1:1080 
 
